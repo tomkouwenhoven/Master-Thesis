4131ff17f709df0956c89aeaeb18df61b9c66465
tomhockey01 <kouwenhoventom@gmail.com>
2021-05-28 16:46:33 +0200

    update

--- Sim_V2/test.sh
+++ Sim_V2/test.sh
@@ -13,29 +13,29 @@
 for i in {1..10}
 do 
     echo "Running with simuluation: $i from gossip 4 and grooming 5"
-    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 3 --gossipagents 3 --gossipeffect 4 --groomeffect 5
+    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 2 --gossipagents 2 --gossipeffect 4 --groomeffect 5
 done 
 
 for i in {1..10}
 do 
     echo "Running with simuluation: $i from gossip 3 and grooming 5"
-    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 3 --gossipagents 3 --gossipeffect 3 --groomeffect 5
+    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 2 --gossipagents 2 --gossipeffect 3 --groomeffect 5
 done 
 
 for i in {1..10}
 do 
     echo "Running with simuluation: $i from gossip 2 and grooming 5"
-    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 3 --gossipagents 3 --gossipeffect 2 --groomeffect 5
+    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 2 --gossipagents 2 --gossipeffect 2 --groomeffect 5
 done 
 
 for i in {1..10}
 do 
     echo "Running with simuluation: $i from gossip 1 and grooming 5"
-    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 3 --gossipagents 3 --gossipeffect 1 --groomeffect 5
+    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 2 --gossipagents 2 --gossipeffect 1 --groomeffect 5
 done 
 
 for i in {1..10}
 do 
     echo "Running with simuluation: $i from gossip 5 and grooming 5"
-    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 3 --gossipagents 3 --gossipeffect 5 --groomeffect 5
+    python __main__.py --nagents 100 --ngroups 20 --nrounds 75 --generations 125 --groomagents 2 --gossipagents 2 --gossipeffect 5 --groomeffect 5
 done 
